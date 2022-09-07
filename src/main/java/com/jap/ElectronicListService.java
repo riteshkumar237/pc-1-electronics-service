38d54a8809950a18637f30346cb84d695abdf1e9
Ritesh Kumar <riteshkumar237@gmail,com>
2022-09-07 23:05:43 +0530

    written method to add all the electronics items inside the List

--- src/main/java/com/jap/ElectronicListService.java
+++ src/main/java/com/jap/ElectronicListService.java
@@ -1,22 +1,35 @@
 package com.jap;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class ElectronicListService {
     //add all the electronics items inside the List
     public static List<String> addElectronicsItemsToList(String electronicsItems) {
         List<String> itemList = null;
+        itemList = new ArrayList<>();
+        ListIterator<String> iterator = (ListIterator<String>) itemList.iterator();
+
+        while (iterator.hasNext()) {
+            String currentItem = iterator.next();
+            itemList.add(currentItem);
+        }
+
         return itemList;
     }
 
     //search the electronic item from the list and return the index position
     public static int searchElectronicItemInList(List<String> itemList, String searchItem) {
+
+
         return -1;
     }
 
     //remove the electronic item from the list using iterator
     public static boolean removeElectronicsItemFromList(List<String> itemList, String removeItem) {
         boolean flag = false;
+
 
         return flag;
     }
